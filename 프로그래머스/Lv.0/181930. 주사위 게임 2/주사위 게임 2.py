b6f25f4c4c88060ba80960b48afdc8a5ea089484
정은채 <ddolchae513@gmail.com>
2024-01-14 23:48:43 +0900

    [level Lv.0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,9 @@
 def solution(a, b, c):
     answer = 0
-    if a == b == c:
+    check = len(set([a,b,c]))
+    if check == 1:
         answer = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-    elif a != b != c != a:
+    elif check == 3:
         answer = a + b + c
     else:
         answer = (a + b + c) * (a**2 + b**2 + c**2)
