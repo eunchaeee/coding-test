1e4c7ea93865433d7edce6c0ce9cd077ad1f6cb5
정은채 <ddolchae513@gmail.com>
2024-02-24 17:44:30 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while(i!=1):
+            if i %2==0:
+                i/=2
+            else:
+                i = (i-1)/2
+            answer+=1
+    return answer
