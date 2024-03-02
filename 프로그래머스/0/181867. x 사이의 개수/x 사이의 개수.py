c7b78ff6ef6a7814766b883c6380bb0849683c01
정은채 <ddolchae513@gmail.com>
2024-03-03 08:07:48 +0900

    [level 0] Title: x 사이의 개수, Time: 1.52 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,11 +1,2 @@
 def solution(myString):
-    answer = []
-    cnt = 0
-    for i in myString:
-        if i =="x":
-            answer.append(cnt)
-            cnt = 0
-        else:
-            cnt+=1
-    answer.append(cnt)
-    return answer
+    return [len(w) for w in myString.split('x')]
