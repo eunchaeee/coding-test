933c279fea137ba9d94895e24d8417051b43f117
정은채 <ddolchae513@gmail.com>
2024-03-10 19:41:56 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -0,0 +1,11 @@
+def solution(arr, flag):
+    X = []
+    
+    for i in range(len(arr)):
+        if flag[i]:
+            for j in range(arr[i] * 2):
+                X.append(arr[i])
+        else:
+            for j in range(arr[i]):
+                X.pop()    
+    return X
