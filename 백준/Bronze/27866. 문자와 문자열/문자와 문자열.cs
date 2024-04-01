941e9c7ee2f38f1802ddb48b9067b0a62664872d
정은채 <ddolchae513@gmail.com>
2024-04-02 00:45:20 +0900

    [Bronze V] Title: 문자와 문자열, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -1,14 +1,3 @@
-using System;
-
-namespace Solution
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        	string S = Console.ReadLine()!;
-            int i = int.Parse(Console.ReadLine()!);
-            Console.WriteLine(S[i-1]);
-        }
-    }
-}
+string S = Console.ReadLine();
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine(S[i-1]);
