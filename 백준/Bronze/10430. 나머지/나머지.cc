1cc354f1e7915eb69915c0264f12ec02237f51ad
정은채 <ddolchae513@gmail.com>
2024-06-02 22:40:37 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.cc
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -1,11 +1,10 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
     int a; int b; int c;
     cin >> a >> b >> c;
-    cout << (a+b)%c << "\n" << ((a%c) + (b%c))%c << "\n" << ((a*b)%c) << "\n" << ((a%c)*(b%c))%c;
+    cout << (a+b)%c << endl << ((a%c) + (b%c))%c << endl << ((a*b)%c) << endl << ((a%c)*(b%c))%c;
     return 0;
 }
