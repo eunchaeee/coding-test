e1a564f64bbfc2e5f2acf2f6b1dd66bf12c570eb
정은채 <ddolchae513@gmail.com>
2024-06-08 16:18:13 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"|\\_/|"<<endl<<"|q p|   /}"<<endl<<"( 0 )\"\"\"\\"<<endl<<"|\"^\"`    |"<<endl<<"||_/=\\\\__|";
+    return 0;
+}
