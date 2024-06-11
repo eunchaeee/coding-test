fb187a51044abb20d93cf1c1e5327f2e8693721c
정은채 <ddolchae513@gmail.com>
2024-06-11 23:28:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin>>x>>y;
+    if (x>0&&y>0)
+    {
+        cout<<1;
+    }
+    else if(x>0&&y<0)
+    {
+        cout<<4;
+    }
+    else if(x<0&&y>0)
+    {
+        cout<<2;
+    }
+    else{
+        cout<<3;
+    }
+    return 0;
+}
