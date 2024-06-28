6b92ebf3a612b51a044da16ce7987849167c6076
정은채 <ddolchae513@gmail.com>
2024-06-29 08:42:52 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if (a!=b && b!=c && c!=a) {
+            answer = a+b+c;
+        }
+        else if (a==b&& b==c){
+            answer = (a+b+c) * (a*a+b*b+c*c) * (a*a*a+b*b*b+c*c*c);
+        }
+        else{
+            answer = (a+b+c) * (a*a+b*b+c*c);
+        }
+        return answer;
+    }
+}
