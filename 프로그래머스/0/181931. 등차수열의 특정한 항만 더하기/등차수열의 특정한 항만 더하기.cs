fcebf3b294142b069b89c2f048c60e1ecb1d8742
정은채 <ddolchae513@gmail.com>
2024-06-28 20:41:04 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.18 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int d, bool[] included) {
+        int answer = 0;
+        for (int i=0; i<included.Length; i++)
+        {
+            if (included[i]) {
+                answer += a+i*d;
+            }    
+        }
+        return answer;
+    }
+}
