def142867a08fc25e221061c837b18cce9341ff0
정은채 <ddolchae513@gmail.com>
2024-06-28 14:03:05 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
 
 ### 성능 요약
 
-메모리: 31.3 MB, 시간: 0.25 ms
+메모리: 31 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 13:17:11
+2024년 06월 28일 14:03:04
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
@@ -2,7 +2,8 @@ using System;
 
 public class Solution {
     public string solution(string my_string, string overwrite_string, int s) {
-        string answer = my_string.Substring(0, s) + overwrite_string + my_string.Substring(s+overwrite_string.Length, my_string.Length-s-overwrite_string.Length);
-        return answer;
+        string result = my_string.Remove(s, overwrite_string.Length);
+        result = result.Insert(s, overwrite_string);
+        return result;
     }
 }
