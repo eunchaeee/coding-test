12b720b68e44bba0630d605e9ec1ff888b34e13f
정은채 <ddolchae513@gmail.com>
2024-08-24 23:08:40 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cc
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -3,22 +3,19 @@ using namespace std;
 
 int main()
 {
-    int h,m;
+    int h; int m;
     cin>>h>>m;
-    if(m>=45)
-    {
+    
+    if(m>=45){
         cout<<h<<" "<<m-45;
     }
-    else
-    {
-        if(h==0)
-        {
-            cout<<23<<" "<<m+15;    
-        }    
-        else
-        {
-            cout<<h-1<<" "<<m+15;    
+    else{
+        if (h==0){
+            cout<<23<<" "<<m+60-45;
+        }
+        else{
+            cout<<h-1<<" "<<m+60-45;
         }
     }
-    return 0;    
+    return 0;
 }
