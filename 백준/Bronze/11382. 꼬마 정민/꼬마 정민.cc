0e00e3097aada65106cf9eac725470719d46129f
정은채 <ddolchae513@gmail.com>
2024-08-26 09:48:28 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     long long a, b, c;
-    cin >> a >> b >> c;
-    cout << a+b+c;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
     return 0;
 }
