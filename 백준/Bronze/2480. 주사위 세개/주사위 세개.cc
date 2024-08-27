f292dda5a09ca03e11d30392281bd2ca8d19dbac
정은채 <ddolchae513@gmail.com>
2024-08-27 20:46:36 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -3,26 +3,21 @@ using namespace std;
 
 int main()
 {
-    int a,b,c;
+    int a, b, c;
     cin>>a>>b>>c;
-    if(a==b&&b==c)
-    {
+    if(a==b&&b==c){
         cout<<10000+a*1000;
     }
-    else if(a!=b&&b!=c&&c!=a)
-    {
+    else if(a!=b&&b!=c&&c!=a){
         cout<<max(max(a,b),c)*100;
     }
-    else
-    {
-        if(a==b)
-        {
+    else{
+        if(a==b||a==c){
             cout<<1000+a*100;
         }
-        else
-        {
-            cout<<1000+c*100;    
-        }       
+        else{
+            cout<<1000+b*100;
+        }
     }
     return 0;
 }
