2b763269d36af3341bc8b139152613900b6faaab
정은채 <ddolchae513@gmail.com>
2024-09-01 15:56:05 +0900

    [Bronze III] Title: 최소, 최대, Time: 384 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int arr[1000000];
 int main()
 {
     int N; cin>>N;
-    for(int i=0;i<N;i++) cin>>arr[i];
     
-    int min_num = 1000000;
-    int max_num = -1000000;
+    int min_result = 1000000;
+    int max_result = -1000000;
     
-    for(int i=0;i<N;i++){
-        if(arr[i]<min_num) min_num=arr[i];
-        if(arr[i]>max_num) max_num=arr[i];
+    for (int i=0; i<N; i++) {
+        int new_num;
+        cin>>new_num;
+        
+        if (min_result > new_num) {
+            min_result = new_num;
+        }
+        if (max_result < new_num) {
+            max_result = new_num;
+        }
     }
     
-    cout<<min_num<<" "<<max_num;
+    cout<<min_result<<" "<<max_result;
+    
     return 0;
 }
