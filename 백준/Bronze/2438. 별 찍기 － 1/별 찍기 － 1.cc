6b0a51e5dc3d1f6df61579902a51f227291c76d1
정은채 <ddolchae513@gmail.com>
2024-09-01 15:44:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -3,15 +3,12 @@ using namespace std;
 
 int main()
 {
-    int a;
-    cin>>a;
-    for(int i=1;i<=a;i++)
-    {
-        for(int j=1; j<=i;j++)
-        {
-            cout<<'*';
-        }
-        cout<<endl;
+    int N;
+    cin>>N;
+    string result="";
+    for (int i=0; i<N; i++) {
+        result+="*";
+        cout<<result<<endl;
     }
     return 0;
 }
