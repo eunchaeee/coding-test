6553ccbdefeb0c4d200cffb762c6ded74fe70414
정은채 <ddolchae513@gmail.com>
2024-09-07 22:39:54 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T; cin>>T;
+    for (int i=1; i<=T; i++){
+        int a, b;
+        cin>>a>>b;
+        cout<<"Case #"<<i<<": "<<a+b<<"\n";
+    }
+    return 0;
+}
