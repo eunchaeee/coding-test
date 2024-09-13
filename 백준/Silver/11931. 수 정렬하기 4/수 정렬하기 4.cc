348d8ea0bf3d2fdbb2cf1d58853fae0bb47fb5d8
정은채 <ddolchae513@gmail.com>
2024-09-13 18:21:24 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 576 ms, Memory: 8176 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> v;
+    
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+    
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << '\n';
+    }
+    
+    return 0;
+}
