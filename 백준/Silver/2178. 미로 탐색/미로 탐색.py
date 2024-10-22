8f1a235247831fb13a82795ca1f3b5ef557fc658
정은채 <ddolchae513@gmail.com>
2024-10-22 21:47:44 +0900

    [Silver I] Title: 미로 탐색, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,24 @@
 import sys
-from collections import deque
 
-input = sys.stdin.readline
+input = lambda : sys.stdin.readline().rstrip()
 
 # N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
 # 2차원 리스트의 맵 정보 입력받기
 graph = []
 for i in range(n):
-    graph.append(list(map(int, input().rstrip())))
+    graph.append(list(map(int, input())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def bfs(x, y):
     # 큐 생성
-    queue = deque([(x, y)])
+    queue = [(x, y)]
     # 큐가 소멸될 때까지 반복
     while (queue):
         # popleft
-        x, y = queue.popleft()
+        x, y = queue.pop(0)
         # 다음 원소들 큐에 넣기
         for i in range(4):
             nx = x + dx[i]
