5bb2717ad80532737204442718d4c94881fce0ad
정은채 <ddolchae513@gmail.com>
2024-11-01 17:03:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31196 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,30 @@
+n = int(input().rstrip())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
+
+def dfs(x, y):
+    house = 0
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return house
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        house += 1
+        house +=  dfs(x+1, y) + dfs(x-1, y) + dfs(x, y-1) + dfs(x, y+1)
+        return house
+    return house
+
+count = 0
+houses = []
+for i in range(n):
+    for j in range(n):
+        h = dfs(i, j)
+        if h != 0:
+            houses.append(h)
+            count += 1
+
+houses.sort()
+print(count)
+for h in houses:
+    print(h)
