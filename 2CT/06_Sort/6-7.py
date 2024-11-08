9faac9cef679f9a2ef5da295276bd48991f72372
EunChae <ddolchae513@gmail.com>
2024-11-08 21:16:24 +0900

    파이썬 정렬 라이브러리

---
+++ 2CT/06_Sort/6-7.py
@@ -0,0 +1,5 @@
+# sorted 내장 함수 : 별도의 리스트를 반환
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+result = sorted(array)
+print(result)
---
+++ 2CT/06_Sort/6-8.py
@@ -0,0 +1,5 @@
+# sort() 내장 함수 : 내부 원소가 바로 정렬됨
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+array.sort()
+print(array)
