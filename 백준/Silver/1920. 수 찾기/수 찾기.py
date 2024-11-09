d103a10b373bca9ac513c2bb76e7d07af9e4c094
정은채 <ddolchae513@gmail.com>
2024-11-09 20:58:28 +0900

    [Silver IV] Title: 수 찾기, Time: 384 ms, Memory: 44812 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def binary_search(arr, t, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == t:
+            return 1
+        elif arr[mid] < t:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return 0
+
+n = int(input().rstrip())
+a = list(map(int, input().rstrip().split()))
+m = int(input().rstrip())
+b = map(int, input().rstrip().split())
+
+a.sort()
+for i in b:
+    print(binary_search(a, i, 0, n - 1))
