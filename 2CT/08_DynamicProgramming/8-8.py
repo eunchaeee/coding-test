a5ca17204e718785427ec7ac7b2e4c4d15e279ac
EunChae <ddolchae513@gmail.com>
2024-11-11 17:07:31 +0900

    DP5 - 효율적인 화폐 구성

---
+++ 2CT/08_DynamicProgramming/8-8.py
@@ -0,0 +1,21 @@
+"""
+5. 효율적인 화폐 구성
+"""
+
+n, m = map(int, input().split())
+arr = []
+d = [10001] * (m+1)
+
+for _ in range(n):
+    arr.append(int(input()))
+
+d[0] = 0
+for i in range(n):
+    for j in range(arr[i], m + 1):
+        if d[j - arr[i]] != 10001:
+            d[j] = min(d[j], d[j - arr[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
