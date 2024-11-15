d71d5aeb5ad9fe7879d312ad9c5900e75ea97c5d
EunChae <ddolchae513@gmail.com>
2024-11-15 16:02:54 +0900

    11.그리디(03.문자열뒤집기) - 백준1439

---
+++ 2CT/11_Greedy/Q03.py
@@ -0,0 +1,15 @@
+"""
+3. 문자열 뒤집기(BOJ1439)
+"""
+count_one = 0
+count_zero = 0
+now = -1
+for i in input():
+    if i == '0' and now != 0:
+        count_zero += 1
+        now = 0
+    elif i == '1' and now != 1:
+        count_one += 1
+        now = 1
+
+print(min(count_one, count_zero))
