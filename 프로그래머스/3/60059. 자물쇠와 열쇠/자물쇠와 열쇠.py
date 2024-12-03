ceb09246c24240fba0b8bcc170dc0ab0b4c1eabd
정은채 <ddolchae513@gmail.com>
2024-12-03 14:02:32 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 802.60 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -0,0 +1,52 @@
+def solution(key, lock):
+    # 자물쇠 확장
+    m = len(key)
+    n = len(lock)
+
+    board = [[0] * (n + 2*m) for _ in range(n + 2*m)]
+    for i in range(n):
+        for j in range(n):
+            board[m + i][m + j] = lock[i][j]
+
+    # 열어보기
+    for i in range(n+m):
+        for j in range(n+m):
+            if compare(key, board, i, j):
+                return True
+
+    for _ in range(3):
+        key = rotate(key)
+        for i in range(n + m):
+            for j in range(n + m):
+                if compare(key, board, i, j):
+                    return True
+
+    return False
+
+def compare(key, board, i, j):
+    m = len(key)
+    n = len(board) - 2 * m
+    result = True
+    for x in range(m):
+        for y in range(m):
+            board[i+x][j+y] += key[x][y]
+
+    for x in range(n):
+        for y in range(n):
+            if board[m + x][m + y] != 1:
+                result = False
+
+    for x in range(m):
+        for y in range(m):
+            board[x+i][y+j] -= key[x][y]
+
+    return result
+
+def rotate(key):
+    m = len(key)
+    result = [[] for _ in range(m)]
+
+    for i in range(m-1, -1, -1):
+        for j in range(m):
+            result[j].append(key[i][j])
+    return result
