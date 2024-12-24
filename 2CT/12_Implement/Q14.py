f7542cde1610b2c55bb1f8f2d4e4da14e77581f1
EunChae <ddolchae513@gmail.com>
2024-12-24 16:24:57 +0900

    몬해먹겠따ㅏㅏㅏ

--- 2CT/12_Implement/Q14.py
+++ 2CT/12_Implement/Q14.py
@@ -4,27 +4,56 @@
 없다면 가장 멀리 떨어진거 버리고 반복하기
 """
 from itertools import combinations
+from collections import deque
+import copy
 
 def solution(n, weak, dist):
     # 간격(apart) 구하기
-    apart = []
+    apart = deque()
     for i in range(len(weak)):
         if i + 1 == len(weak):
             apart.append(n - weak[i] + weak[0])
         else:
             apart.append(weak[i+1] - weak[i])
 
-    print(apart)
     # dist에 원소가 남아있으면 계속 진행
     while dist:
         # 가장 큰 dist 꺼내기
         current_dist = dist.pop()
-        sum = 0
         count = 0
         indices = []
-        # apart의 첫 원소부터 계산
+        # apart의 첫 원소부터 계산해서 current_dist 를 안넘는 가장많은 원소로 이루어진 숫자쌍 구하기
+        print("current_dist ", current_dist)
+        for i in range(len(apart)):
+            copy_apart = copy.deepcopy(apart)
+            copy_apart.rotate(-1)
+            print(copy_apart)
+            sum = 0
+            apart_elements = []
+            for j in range(len(copy_apart)):
+                temp_sum = 0
+                temp_apart_elements = []
+                if temp_sum + copy_apart[j] > current_dist:
+                    print(sum, apart_elements)
+                    if len(apart_elements) > len(temp_apart_elements):
+                        sum = temp_sum
+                        apart_elements = temp_apart_elements
+                    elif len(apart_elements) == len(temp_apart_elements):
+                        print("!!!", copy_apart)
+
+                    break
+                else:
+                    copy_apart.remove(copy_apart[j])
+                    temp_sum += copy_apart[j]
+                    apart_elements.append(copy_apart[j])
+
+
+        return
+
         for start in range(len(apart)):
+            sum = 0
             i = 0
+            num_list = []
             # 여기서 start + i 가 전체 인덱스 개수보다 커졌을 때 0부터 초기화되도록 해야하고, 이때 초기화한 인덱스가 start를 넘어서면 지금까지 사용한? 친구의 수만큼 return.
             while True:
                 # 다음 원소를 더한 것이 현재 친구의 이동거리(current_dist)보다 커지면 break
@@ -33,7 +62,9 @@ def solution(n, weak, dist):
                 # 그렇지 않으면 다음 원소도 더한다.
                 else:
                     sum += apart[start + i]
+                    num_list.append(apart[start + i])
                     i += 1
+            print(dist, start, start+i, sum, num_list)
             # i+1는 원소 개수가 됨.
             # indices = list(range(start, i+1))
             # if count < i + 1:
@@ -97,4 +128,4 @@ def solution_old(n, weak, dist):
 
 
 print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4])) # 2
-print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7])) # 1
+#print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7])) # 1
