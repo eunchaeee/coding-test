73f9d0caa7f342e0f417c9267a588e9a9b8ae0be
eunchae <ddolchae513@gmail.com>
2025-05-02 17:08:39 +0900

    [HackerRank] [C#] Insertion Sort Part1

---
+++ CSharp/HackerRank/TutorialChallenges/InsertionSort/InsertionSort_Part1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'insertionSort1' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort1(int n, List<int> arr)
+    {
+        int target = arr[n - 1];
+        for (int current = n - 1; current >= 0; current--)
+        {
+            if (current == 0)
+            {
+                arr[0] = target;
+                Console.WriteLine(string.Join(' ', arr));
+                break;
+            }
+            
+            if (arr[current - 1] >= target)
+            {
+                arr[current] = arr[current - 1];
+                Console.WriteLine(string.Join(' ', arr));
+            }
+            else
+            {
+                arr[current] = target;
+                Console.WriteLine(string.Join(' ', arr));
+                break;
+            }
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.insertionSort1(n, arr);
+    }
+}
+
+// 10
+// 2 3 4 5 6 7 8 9 10 1
---
+++ CSharp/HackerRank/TutorialChallenges/InsertionSort/InsertionSort_Part1/README.md
@@ -0,0 +1,10 @@
+# Hacker Rank : Insertion Sort Part 1
+
+링크 : https://www.hackerrank.com/challenges/insertionsort1
+
+### 삽입정렬
+
+시간복잡도가 정렬된 정도에 따라 O(n) ~ O(n제곱)까지!!
+
+### 구현 포인트
+- 마지막 current = 0 일때를 빼먹지말자!
