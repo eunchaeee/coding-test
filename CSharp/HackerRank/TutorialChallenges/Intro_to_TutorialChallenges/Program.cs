5bf2066f1b937fd6cbbe1ab4b3e7cf0cdcb6e09e
eunchae <ddolchae513@gmail.com>
2025-05-02 16:17:10 +0900

    [HackerRank] [C#] Intro to TutorialChallenges

---
+++ CSharp/HackerRank/TutorialChallenges/Intro_to_TutorialChallenges/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'introTutorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER V
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int introTutorial(int V, List<int> arr)
+    {
+        return arr.IndexOf(V);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int V = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.introTutorial(V, arr);
+
+        Console.WriteLine(result);
+    }
+}
