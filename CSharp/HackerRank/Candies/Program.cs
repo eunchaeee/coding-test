8b9b34083241ab7f3628ba96fde47204cf5ff8f0
eunchae <ddolchae513@gmail.com>
2025-05-03 21:06:56 +0900

    [HackerRank] [C#] Candies
    어제 + 3트까지 하고 결국 풀이 찾아보고 풀었다.

--- CSharp/HackerRank/Candies/Program.cs
+++ CSharp/HackerRank/Candies/Program.cs
@@ -26,7 +26,39 @@ class Result
 
     public static long candies(int n, List<int> arr)
     {
-        return 0;
+        int[] candies = new int[n];
+        
+        // 1개 씩 주기
+        for (int i = 0; i < n; i++)
+        {
+            candies[i] = 1;
+        }
+        
+        // 왼쪽 -> 오른쪽 : 오른쪽이 더 크면 오른쪽에 사탕 하나 더주기
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] > arr[i - 1])
+            {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        
+        // 오른쪽 -> 왼쪽 : 오른쪽이 더 작으면 왼쪽에 사탕 하나 더주기
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (arr[i + 1] < arr[i])
+            {
+                candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        long answer = 0;
+        foreach (int c in candies)
+        {
+            answer += c;
+        }
+
+        return answer;
     }
 
 }
