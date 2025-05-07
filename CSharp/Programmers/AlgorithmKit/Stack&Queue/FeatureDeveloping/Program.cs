7f6cf8471bdc585e50f2c0aead391bbf4bb69a9f
eunchae <ddolchae513@gmail.com>
2025-05-07 20:16:51 +0900

    [프로그래머스 고득점 Kit] [스택/큐] [C#] 기능개발

---
+++ CSharp/Programmers/AlgorithmKit/Stack&Queue/FeatureDeveloping/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<int> answer = new List<int>();
+        int n = progresses.Length;
+        int[] days = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            days[i] = ((100 - progresses[i]) + (speeds[i] - 1)) / speeds[i];
+        }
+
+        int prev = 0;
+        while (prev < n)
+        {
+            int follow = prev + 1;
+            while (follow < n && days[follow] <= days[prev])
+            {
+                follow++;
+            }
+            answer.Add(follow - prev);
+            prev = follow;
+        }
+        
+        return answer.ToArray();
+    }
+
+    public static void Main(string[] args)
+    {
+        var sol = new Solution();
+        int[] progress1 = [93, 30, 55];
+        int[] speeds1 = [1, 30, 5];
+        int[] progress2 = [95, 90, 99, 99, 80, 99];
+        int[] speeds2 = [1, 1, 1, 1, 1, 1];
+
+        sol.solution(progress1, speeds1);
+        sol.solution(progress2, speeds2);
+    }
+}
---
+++ CSharp/Programmers/AlgorithmKit/Stack&Queue/FeatureDeveloping/README.md
@@ -0,0 +1,8 @@
+# 프로그래머스 코딩테스트 고득점 Kit
+# [스택/큐] 기능개발
+
+## 링크
+https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+## 알아두기
+- 작업 리스트가 선입선출 구조여서 [스택/큐] 문제로 분류!
