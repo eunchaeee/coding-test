8b34cefe659a9478f89c8cbd7c5f2d5f1d28de95
eunchae <ddolchae513@gmail.com>
2025-05-07 23:37:23 +0900

    [프로그래머스 고득점 Kit] [DFS/BFS] [C#] 타겟 넘버

---
+++ CSharp/Programmers/AlgorithmKit/DFS&BFS/TargetNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int[] numbers, int target) {
+        return DFS(0, numbers, 0, target);
+    }
+
+    public int DFS(int now, int[] numbers, int sum, int target)
+    {
+        // If now is on end index
+        if (now == numbers.Length)
+        {
+            return sum == target ? 1 : 0;
+        }
+        
+        int plus = DFS(now + 1, numbers, sum + numbers[now], target);
+        int minus = DFS(now + 1, numbers, sum - numbers[now], target);
+        
+        return plus + minus;
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution([1, 1, 1, 1, 1], 3));
+        Console.WriteLine(sol.solution([4, 1, 2, 1], 4));
+    }
+}
