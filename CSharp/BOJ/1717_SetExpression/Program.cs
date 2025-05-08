1d501f73f4fa4d198804e8efd0c80acbb87ab229
eunchae <ddolchae513@gmail.com>
2025-05-08 15:51:13 +0900

    [백준] [C#] 1717. 집합의 표현
    Find 함수, Union 함수

---
+++ CSharp/BOJ/1717_SetExpression/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    private static int[] parent;
+
+    static void Main()
+    {
+        var input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++)
+        {
+            var cmd = Console.ReadLine().Split();
+            int op = int.Parse(cmd[0]);
+            int a = int.Parse(cmd[1]);
+            int b = int.Parse(cmd[2]);
+
+            if (op == 0)
+            {
+                Union(a, b);
+            }
+            else
+            {
+                if (Find(a) == Find(b))
+                {
+                    sb.AppendLine("YES");
+                }
+                else
+                {
+                    sb.AppendLine("NO");
+                }
+            }
+        }
+        Console.Write(sb.ToString());
+    }
+
+    static int Find(int x)
+    {
+        if (parent[x] != x)
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
+    
+    static void Union(int a, int b)
+    {
+        int rootA = Find(a);
+        int rootB = Find(b);
+
+        if (rootA != rootB)
+        {
+            if (rootA < rootB)
+            {
+                parent[rootB] = rootA;
+            }
+            else
+            {
+                parent[rootA] = rootB;
+            }
+        }
+    }
+}
---
+++ CSharp/BOJ/1717_SetExpression/README.md
@@ -0,0 +1,12 @@
+# 백준 1717. 집합의 표현
+
+
+## 링크
+https://www.acmicpc.net/problem/1717
+
+
+### Find 함수
+parent 리스트를 사용해서 최종 parent을 반환
+
+### Union 함수
+a, b 중 parent 리스트의 값을 큰 원소의 parent값을 작은 원소의 parent 값으로 변경해줌.
