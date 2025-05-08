e27596b8a22d3399abbdd95b816be521893bd4be
eunchae <ddolchae513@gmail.com>
2025-05-08 22:23:02 +0900

    [백준] [C#] 10451. 순열 사이클

---
+++ CSharp/BOJ/10451_PermutationCycle/Program.cs
@@ -0,0 +1,61 @@
+﻿
+static void Main()
+{
+    int t = int.Parse(Console.ReadLine());
+    for (int i = 0; i < t; i++)
+    {
+        GetCycleCount();
+    }
+}
+
+static int GetCycleCount()
+{
+    int cycleCount = 0;
+
+    int n = int.Parse(Console.ReadLine());
+    var data = Console.ReadLine().Split().Select((x) => int.Parse(x)).ToArray();
+    
+    List<int>[] graph = new List<int>[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        graph[i].Add(data[i]);
+        graph[data[i]].Add(i);
+    }
+
+    int[] depth = new int[n + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+        depth[i] = -1;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (depth[i] != -1) continue;
+
+        depth[i] = 0;
+        if (IsCycle(i, graph, depth))
+        {
+            cycleCount++;
+        }
+    }
+    
+    
+
+    return cycleCount;
+}
+
+static bool IsCycle(int now, List<int>[] graph, int[] depth)
+{
+    if (depth[now] == 0)
+    {
+        return true;
+    }
+
+    foreach (var neighbor in graph[now])
+    {
+        depth[neighbor] = depth[now] + 1;
+        var result = IsCycle(neighbor, graph, depth);
+    }
+    
+}
