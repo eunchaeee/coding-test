b4a67188c290085effbe156fdc4754ed8849b668
eunchae <ddolchae513@gmail.com>
2025-05-08 22:23:45 +0900

    [프로그래머스 고득점 Kit] [BFS/DFS] [C#] 네트워크

---
+++ CSharp/Programmers/AlgorithmKit/DFS&BFS/Network/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int n, int[,] computers) {
+        int answer = 0;
+        bool[] visited = new bool[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (DFS(i, computers, visited))
+            {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public bool DFS(int now, int[,] computers, bool[] visited)
+    {
+        if (visited[now]) return false;
+
+        visited[now] = true;
+        
+        for (int i = 0; i < computers.GetLength(0); i++)
+        {
+            if (i == now) continue;
+
+            if (computers[now, i] == 1)
+            {
+                DFS(i, computers, visited);
+            }
+        }
+
+        return true;
+    }
+
+    public static void Main()
+    {
+        var sol = new Solution();
+        var computers1 = new int[,] { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+        var computers2 = new int[,] { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } };
+        Console.WriteLine(sol.solution(3, computers1));
+        Console.WriteLine(sol.solution(3, computers2));
+    }
+}
---
+++ CSharp/Programmers/AlgorithmKit/DFS&BFS/Network/README.md
@@ -0,0 +1,5 @@
+# 프로그래머스 코딩테스트 고득점 Kit
+# [DFS/BFS] 네트워크
+
+### 링크
+https://school.programmers.co.kr/learn/courses/30/lessons/43162
