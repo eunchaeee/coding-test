9fe1cd8c6cdf3145afc6eff2930e410a49468ccd
eunchae <ddolchae513@gmail.com>
2025-05-19 11:46:49 +0900

    [LeetCode] [C#] 415. Add Strings

---
+++ CSharp/LeetCode/415_AddStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution {
+    public string AddStrings(string num1, string num2)
+    {
+        int maxLen = num1.Length > num2.Length ? num1.Length : num2.Length;
+        int[] answer = new int[maxLen + 1];
+        for (int i = 0; i < maxLen; i++)
+        {
+            int sum = 0;
+            int idx1 = (num1.Length - 1) - i;
+            int idx2 = (num2.Length - 1) - i;
+            
+            if (idx1 >= 0)
+                sum += num1[num1.Length - i - 1] - '0';
+            
+            if (idx2 >= 0)
+                sum += num2[num2.Length - i - 1] - '0';
+
+            answer[i] += sum;
+            if (answer[i] >= 10)
+            {
+                answer[i] -= 10;
+                answer[i + 1] += 1;
+            }
+        }
+        
+        Array.Reverse(answer);
+        string result = string.Join("", answer.Select(n => n.ToString())).TrimStart('0');
+
+        if (result == "") result = "0";
+        return result;
+    }
+
+    public static void Main()
+    {
+        var sol = new Solution();
+        sol.AddStrings("11", "123");
+    }
+}
---
+++ CSharp/LeetCode/415_AddStrings/README.md
@@ -0,0 +1,8 @@
+# LeetCode 415. Add Strings
+
+### 링크
+https://leetcode.com/problems/add-strings/description/
+
+### Tip
+- 엄청 큰 수의 덧셈
+- Big Integer
