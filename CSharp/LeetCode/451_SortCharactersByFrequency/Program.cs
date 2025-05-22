9b1b9a8a4646681577cb26597e5859453354d50c
eunchae <ddolchae513@gmail.com>
2025-05-22 11:54:10 +0900

    [LeetCode] [C#] 451.Sort Characters By Frequency

---
+++ CSharp/LeetCode/451_SortCharactersByFrequency/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+public class Solution {
+    public string FrequencySort(string s)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (dict.ContainsKey(c))
+            {
+                dict[c]++;
+            }
+            else
+            {
+                dict.Add(c, 1);
+            }
+        }
+
+        var sorted = dict.OrderByDescending(pair => pair.Value).ToList();
+
+        string answer = "";
+        foreach (var pair in sorted)
+        {
+            answer += string.Concat(Enumerable.Repeat(pair.Key, pair.Value));
+        }
+
+        return answer;
+    }
+
+    public static void Main()
+    {
+        var sol = new Solution();
+        Console.WriteLine(sol.FrequencySort("tree"));
+    }
+}
---
+++ CSharp/LeetCode/451_SortCharactersByFrequency/README.md
@@ -0,0 +1,5 @@
+# LeetCode 451. Sort Characters By Frequency
+
+### 링크
+https://leetcode.com/problems/sort-characters-by-frequency/description/
+
