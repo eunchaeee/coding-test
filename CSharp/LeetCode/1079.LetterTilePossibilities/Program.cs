43a30999facb4b5480a98751d8c3ae7179324a3c
eunchae <ddolchae513@gmail.com>
2025-05-22 13:04:49 +0900

    [LeetCode] [C#] 1079.LetterTilePossibilities

---
+++ CSharp/LeetCode/1079.LetterTilePossibilities/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public int NumTilePossibilities(string tiles)
+    {
+        HashSet<string> comb = new HashSet<string>();
+        List<int> path = new List<int>();
+        bool[] visited = new bool[tiles.Length];
+        
+        DFS(tiles, path, visited, comb);
+        return comb.Count;
+    }
+
+    public void DFS(string tiles, List<int> path, bool[] visited, HashSet<string> comb)
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            path.Add(tiles[i]);
+            comb.Add(string.Join("", path));
+            
+            DFS(tiles, path, visited, comb);
+
+            visited[i] = false;
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+
+    public static void Main()
+    {
+        var sol = new Solution();
+        Console.WriteLine(sol.NumTilePossibilities("AAB"));
+        Console.WriteLine(sol.NumTilePossibilities("AAABBC"));
+        Console.WriteLine(sol.NumTilePossibilities("V"));
+    }
+}
---
+++ CSharp/LeetCode/1079.LetterTilePossibilities/README.md
@@ -0,0 +1,8 @@
+# LeetCode 1079. Letter Tile Possibilities
+
+### 링크
+https://leetcode.com/problems/letter-tile-possibilities/description/
+
+### Tips
+- 백트래킹 DFS!!!!
+- path 리스트를 만들어서 포문 안에서 해당 문자 추가하고 DFS 돌리고 마지막에 그 문자 빼주는 방식!
