d94323a403898115ec2dccd02cc74ffb0cf7e22d
eunchae <ddolchae513@gmail.com>
2025-05-30 00:15:06 +0900

    [LeetCode] [C#] 2305. Fair Distribution of Cookies
    백트래킹....후우

---
+++ CSharp/LeetCode/2305_FairDistributionOfCookies/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    private int minUnfairness = int.MaxValue;
+    private int count = 0;
+    public int DistributeCookies(int[] cookies, int k)
+    {
+        int[] kids = new int[k];
+        BackTracking(cookies, kids, 0);
+        return minUnfairness;
+    }
+
+    public void BackTracking(int[] cookies, int[] kids, int idx)
+    {
+        count++;
+        Console.WriteLine($"\n[BackTracking{count}] Cookie #{idx}");
+        if (idx == cookies.Length)
+        {
+            int unfairness = kids.Max();
+            minUnfairness = Math.Min(unfairness, minUnfairness);
+            Console.WriteLine($"!!!Unfairness Update!!! {minUnfairness}");
+            return;
+        }
+
+        for (int i = 0; i < kids.Length; i++)
+        {
+            Console.WriteLine($"Cookie #{idx} (value = {cookies[idx]}) to kids '{i}'");
+            kids[i] += cookies[idx];
+            Console.WriteLine($"kids : {string.Join(" ", kids)}");
+
+            if (kids[i] < minUnfairness)
+            {
+                BackTracking(cookies, kids, idx + 1);
+            }
+
+            kids[i] -= cookies[idx];
+            if (kids[i] == 0) break;
+            Console.WriteLine($"@@@BackTrack: kids'{i}' - Cookie #{idx} (value = {cookies[idx]}) => {string.Join(" ", kids)}");
+        }
+    }
+    
+    private static void Main()
+    {
+        var sol = new Solution();
+        Console.WriteLine(sol.DistributeCookies([8, 15, 10, 20, 8], 2));
+    }
+}
---
+++ CSharp/LeetCode/2305_FairDistributionOfCookies/README.md
@@ -0,0 +1,9 @@
+# LeetCode 2305. Fair Distribution of Cookies
+
+## 링크
+https://leetcode.com/problems/fair-distribution-of-cookies/description/
+
+## Back Tracking
+- 진짜 너무 어려움.... 그치만 이해 어느정도했다..!
+- 백트래킹은 조건식을 넣어서 커트 해줬을 때 속도 차이가 많이 난다.
+- 조건식 넣어줬을 때 5ms, 안 넣었을 때 2218ms였음...!
