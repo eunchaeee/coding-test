2e357bf283e76cd7efe01597b1a20aab8371cacf
eunchae <ddolchae513@gmail.com>
2025-06-05 08:55:52 +0900

    [LeeetCode] [C#] 1509.Min Diff in 3 Moves

---
+++ LeetCode/1509_MinDiffIn3Moves/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution 
+{
+    public int MinDifference(int[] nums)
+    {
+        int n = nums.Length;
+
+        if (n <= 4) return 0;
+        
+        Array.Sort(nums);
+
+        int apart = n - 4;
+        int minDiff = int.MaxValue;
+        for (int i = 0; i < n - apart; i++)
+        {
+            for (int j = i + apart; j < n; j++)
+            {
+                minDiff = Math.Min(minDiff, nums[j] - nums[i]);
+            }
+        }
+
+        return minDiff;
+    }
+
+    public static void Main()
+    {
+        var sol = new Solution();
+        Console.WriteLine(sol.MinDifference([1, 5, 0, 10, 14]));
+    }
+}
---
+++ LeetCode/1509_MinDiffIn3Moves/README.md
@@ -0,0 +1,11 @@
+# LeetCode 1509 : Minimum Difference Between Largest and Smallest Value in Three Moves
+
+## 링크
+https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/description/
+
+## 방법
+정렬시킨 후 앞에서부터 값을 비교한다.
+
+
+
+소요시간 23분(왤케 잘풀지?!!!)
