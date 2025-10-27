25d91ffe7f51a8f28dbe4eb6603cfbdc4c7dc4a0
eunchae <ddolchae513@gmail.com>
2025-10-27 23:09:09 +0900

    [프로그래머스] [Lv.1] 카드 뭉치

---
+++ CSharp/Programmers/Deck/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Solution {
+    public string solution(string[] cards1, string[] cards2, string[] goal) {
+
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for (int i = 0; i < goal.Length; i++)
+        {
+            if (idx1 < cards1.Length && goal[i] == cards1[idx1])
+            {
+                idx1++;
+            }
+            else if (idx2 < cards2.Length && goal[i] == cards2[idx2])
+            {
+                idx2++;
+            }
+            else
+            {
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+
+        string[] cards1 = new string[] {"i", "drink", "water" };
+        string[] cards2 = new string[] { "want", "to" };
+        string[] cards3 = new[] { "i", "water", "drink" };
+
+        string[] goal = ["i", "want", "to", "drink", "water"];
+        
+        
+        Console.WriteLine(sol.solution(cards1, cards2, goal));
+        Console.WriteLine(sol.solution(cards3, cards2, goal));
+    }
+}
+
