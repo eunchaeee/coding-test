ac5bfa28b655782b649586981098652753c1e6f5
eunchae <ddolchae513@gmail.com>
2025-10-29 11:05:49 +0900

    [LeetCode] 1190. Reverse Substrings Between Each Pair of Parentheses
    괄호 짝 매핑, 방향 전환, StringBuilder

---
+++ CSharp/LeetCode/1190.ReverseSubstringsBetweenEachPairOfParentheses/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Text;
+
+public class Solution 
+{
+    public string ReverseParentheses(string s)
+    {
+        int n = s.Length;
+        int[] pair = new int[n];
+        var st = new Stack<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '(') st.Push(i);
+            else if (s[i] == ')')
+            {
+                int j = st.Pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+
+        var sb = new StringBuilder(n);
+        for (int i = 0, dir = 1; i < n; i += dir)
+        {
+            char c = s[i];
+            if (c == '(' || c == ')')
+            {
+                i = pair[i];
+                dir = -dir;
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.ReverseParentheses("ta()usw((((a))))"));
+    }
+}
