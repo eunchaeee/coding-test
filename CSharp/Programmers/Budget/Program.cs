e5b14fde8284cdf909d8ba6c19ace71087d139c6
eunchae <ddolchae513@gmail.com>
2025-11-06 17:38:41 +0900

    [프로그래머스] 예산

---
+++ CSharp/Programmers/Budget/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int[] d, int budget) {
+        Array.Sort(d);
+        int count = 0;
+        for (int i = 0; i < d.Length; i++)
+        {
+            if (budget - d[i] < 0)
+            {
+                break;
+            }
+
+            budget -= d[i];
+            count++;
+        }
+        return count;
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+        int[] d1 = new int[5] { 1, 3, 2, 5, 4 };
+        int[] d2 = new int[4] { 2, 2, 3, 3 };
+        sol.solution(d1, 9);
+        sol.solution(d2, 10);
+    }
+}
