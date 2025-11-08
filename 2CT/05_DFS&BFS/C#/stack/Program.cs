ea93221aa1c489a25648e5987ce6f580e14ba41e
eunchae <ddolchae513@gmail.com>
2025-11-08 10:31:56 +0900

    [이테코] C# 스택 구현 - Stack, List

---
+++ 2CT/05_DFS&BFS/C#/stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public void solution()
+    {
+        // Stack : Push() / Pop()
+        Stack<int> s = new Stack<int>();
+        s.Push(1);
+        s.Push(2);
+        s.Pop();
+
+        List<int> l = new List<int>();
+        l.Add(1);
+        l.Add(2);
+        l.Add(3);
+        l.RemoveAt(l.Count - 1);
+        l.RemoveAt(l.Count - 1);
+        Console.WriteLine(l);
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+        sol.solution();
+    }
+}
