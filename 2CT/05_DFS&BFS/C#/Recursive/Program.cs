172139b0d1fe3b8147af90538ba765dedce88945
eunchae <ddolchae513@gmail.com>
2025-11-08 11:02:56 +0900

    [이테코] C# 재귀함수

---
+++ 2CT/05_DFS&BFS/C#/Recursive/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        SayHi(5);
+    }
+
+    static void SayHi(int count)
+    {
+        if (count <= 0)
+            return;
+        Console.WriteLine($"Hi {count}");
+        SayHi(count - 1 );
+        Console.WriteLine($"End {count}");
+    }
+}
