c2d6bca84975bb63c4294c1a1212a85d139701d4
eunchae <ddolchae513@gmail.com>
2025-11-08 10:53:58 +0900

    [이테코] C# Queue

---
+++ 2CT/05_DFS&BFS/C#/Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Queue
+{
+    public static void Main()
+    {
+        Queue<int> queue = new Queue<int>();
+        
+        // Enqueue: 데이터 추가
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        
+        // Peek: 맨 앞 확인 (꺼내지 않음)
+        Console.WriteLine(queue.Peek());
+        
+        // Dequeue: 맨 앞 꺼내기
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        
+        Console.WriteLine(queue.Count);
+    }
+}
