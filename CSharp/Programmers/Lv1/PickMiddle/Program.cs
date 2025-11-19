a15e66b78ab18cd829d64d65e3ced761d0fdb069
eunchae <ddolchae513@gmail.com>
2025-11-19 10:05:34 +0900

    [프로그래머스] [Lv1] [C#] 가운데 글자 가져오기
    Substring

---
+++ CSharp/Programmers/Lv1/PickMiddle/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static string solution(string s)
+    {
+        string answer = "";
+        int l = s.Length;
+
+        if (l % 2 == 1)
+        {
+            answer = s.Substring(l / 2, 1);
+        }
+        else
+        {
+            answer = s.Substring(l / 2 - 1,2);
+        }
+        return answer;
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine(solution("abcde"));
+        Console.WriteLine(solution("qwer"));
+        
+    }
+}
