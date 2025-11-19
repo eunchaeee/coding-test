83180d9aeef41239beb81c6e339ccefc435057d6
eunchae <ddolchae513@gmail.com>
2025-11-19 13:40:32 +0900

    [프로그래머스] [Lv.1] [C#] 문자열 내 마음대로 정렬하기

---
+++ CSharp/Programmers/Lv1/SortArray/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public string[] solution2(string[] strings, int n)
+    {
+        List<string> answer = new List<string>();
+        var pq = new PriorityQueue<string, (char p1, string p2)>();
+
+        foreach (string s in strings)
+        {
+            pq.Enqueue(s, (s[n], s));
+        }
+
+        while (pq.Count > 0)
+        {
+            answer.Add(pq.Dequeue());
+        }
+        
+        return answer.ToArray();
+    }
+    
+    public string[] solution(string[] strings, int n)
+    {
+        Dictionary<char, List<string>> dict = new Dictionary<char, List<string>>();
+
+        foreach (string s in strings)
+        {
+            if (dict.ContainsKey(s[n]))
+            {
+                dict[s[n]].Add(s);
+            }
+            else
+            {
+                dict[s[n]] = new List<string>() { s };
+            }
+        }
+
+        List<string> answer = new List<string>();
+        
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            if (dict.ContainsKey(i))
+            {
+                var temp = dict[i];
+                temp.Sort();
+                answer.AddRange(temp);
+            }
+        }
+
+        return answer.ToArray();
+    }
+
+    static void Main()
+    {
+        
+    }
+}
