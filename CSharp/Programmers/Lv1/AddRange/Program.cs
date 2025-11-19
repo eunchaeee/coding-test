1eb08bca9f7d717fedb049760778f1efb3dfe3b6
eunchae <ddolchae513@gmail.com>
2025-11-19 10:29:33 +0900

    [프로그래머스] [Lv.1] [C#] 두 정수 사이의 합

---
+++ CSharp/Programmers/Lv1/AddRange/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    public long solution(int a, int b) 
+    {
+        long answer = 0;
+
+        if (a < b)
+        {
+            answer = AddRange(a, b);
+        }
+        else if (a > b)
+        {
+            answer = AddRange(b, a);
+        }
+        else
+        {
+            answer = a;
+        }
+        
+        return answer;
+    }
+
+    public long AddRange(int first, int second)
+    {
+        long answer = 0;
+        for (int i = first; i < second + 1; i++)
+        {
+            answer += i;
+        }
+
+        return answer;
+    }
+    
+    
+    static void Main()
+    {
+        Program pg = new Program();
+        Console.WriteLine(pg.solution(3, 5));
+        Console.WriteLine(pg.solution(3, 3));
+        Console.WriteLine(pg.solution(5, 3));
+    }
+}
