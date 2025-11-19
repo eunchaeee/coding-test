384b04df59bb4712943c2b22c28d46c52026579e
eunchae <ddolchae513@gmail.com>
2025-11-19 15:24:41 +0900

    [프로그래머스] [Lv.1] [C#] 문자열 다루기 기본

---
+++ CSharp/Programmers/Lv1/StringHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution {
+    public static bool solution(string s) {
+        bool answer = true;
+        if (s.Length == 4 || s.Length == 6)
+        {
+            foreach (char c in s)
+            {
+                if (c < '0' || c > '9')
+                {
+                    answer = false;
+                }
+            }
+        }
+        else
+        {
+            answer = false;
+        }
+        
+        return answer;
+    }
+
+    public static bool solution2(string s)
+    {
+        bool answer = false;
+        if (s.Length == 4 || s.Length == 6)
+            answer = int.TryParse(s, out int i);
+        return answer;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solution("a234"));
+        Console.WriteLine(solution("1234"));
+        Console.WriteLine(solution2("a234"));
+        Console.WriteLine(solution2("1234"));
+    }
+}
