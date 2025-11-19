fb727857e7ed105d8925094c02244ad0aae9cff0
eunchae <ddolchae513@gmail.com>
2025-11-19 14:47:33 +0900

    [프로그래머스] [Lv.1] [C#] 문자열 내림차순으로 배치하기

---
+++ CSharp/Programmers/Lv1/DescendingOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static string solution(string s)
+    {
+        string answer = "";
+        List<char> chars = new List<char>();
+        foreach (char c in s)
+        {
+            chars.Add(c);
+        }
+        chars.Sort();
+        var sorted = chars.OrderByDescending(x => x);
+        answer = string.Join("", sorted);
+        return answer;
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine(solution("Zbcdefg"));   
+    }
+}
