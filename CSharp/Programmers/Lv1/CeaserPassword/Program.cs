5641545d0573d0be7ed219a4375f35c92d558985
eunchae <ddolchae513@gmail.com>
2025-11-20 10:47:29 +0900

    [프로그래머스] [Lv.1] [C#] 시저 암호
    유니코드

---
+++ CSharp/Programmers/Lv1/CeaserPassword/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution {
+    public string solution(string s, int n) {
+        string answer = "";
+        int alphabet_count = 26;
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            int encode = s[i] + n;
+            // 소문자
+            if ('a' <= s[i] && s[i] <= 'z')
+            {
+                answer += encode > 'z' ? (char)(encode - alphabet_count) : (char)(encode);
+            }
+            else if ('A' <= s[i] && s[i] <= 'Z')
+            {
+                answer += encode > 'Z' ? (char)(encode - alphabet_count) : (char)(encode);
+            }
+            else
+            {
+                answer += " ";
+            }
+            Console.WriteLine($"{encode}, {answer}");
+        }
+        return answer;
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution("AB", 1));
+        Console.WriteLine(sol.solution("z", 1));
+        Console.WriteLine(sol.solution("a B z", 4));
+    }
+}
