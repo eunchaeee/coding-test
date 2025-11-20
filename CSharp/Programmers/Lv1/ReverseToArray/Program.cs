729623b10a808f78aebdebc1da93946b70a7ffee
eunchae <ddolchae513@gmail.com>
2025-11-20 13:42:37 +0900

    [프로그래머스] [Lv.1] [C#] 자연수 뒤집어 배열로 만들기

---
+++ CSharp/Programmers/Lv1/ReverseToArray/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public static int[] solution(long n)
+    {        
+        string s = n.ToString();
+        int[] answer = new int[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int.TryParse(s.Substring(s.Length - 1 - i, 1), out int x);
+            answer[i] = x;
+        }
+        
+        return answer;
+    }
+
+    static void Main()
+    {
+        solution(12345);
+    }
+}
