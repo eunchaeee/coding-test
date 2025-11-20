d5f9ce0eede4d00856ea592ffb21397495ca8e7d
eunchae <ddolchae513@gmail.com>
2025-11-20 10:20:19 +0900

    [프로그래머스] [Lv.1] [C#] 문자열을 정수로 바꾸기

---
+++ CSharp/Programmers/Lv1/stringtoint/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public static int solution(string s) 
+    {
+        int answer = 0;
+        int.TryParse(s, out answer);
+        return answer;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(solution("1234"));
+        Console.WriteLine(solution("-1234"));
+    }
+}
