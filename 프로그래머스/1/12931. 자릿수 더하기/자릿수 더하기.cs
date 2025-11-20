de63243fda0ddbc43a4117b5db855dd89d4ab78b
정은채 <ddolchae513@gmail.com>
2025-11-20 13:34:20 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.05 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int n) 
+    {
+        int answer = 0;
+        string s = n.ToString();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int.TryParse(s.Substring(i, 1), out int x);
+            answer += x;
+        }
+        
+        return answer;
+    }
+}
