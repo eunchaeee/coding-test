15787cd7aa4e0b186caf9770c35d6e4e0ecd70db
eunchae <ddolchae513@gmail.com>
2025-11-20 10:13:47 +0900

    [프로그래머스] [Lv.1] [C#] 수박수박수박수박수박수?

---
+++ CSharp/Programmers/Lv1/SBSBSBSBSBS/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public static string solution(int n) {
+        string answer = "";
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+                answer += "수";
+            else
+                answer += "박";
+        }
+        return answer;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(solution(3));
+        Console.WriteLine(solution(4));
+    }
+}
