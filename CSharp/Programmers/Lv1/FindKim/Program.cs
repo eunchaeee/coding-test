65f4596f64f40d509ff98d762219a913332292d9
eunchae <ddolchae513@gmail.com>
2025-11-20 09:46:38 +0900

    [프로그래머스] [Lv1.] [C#] 소수 찾기
    효율성 올리기

---
+++ CSharp/Programmers/Lv1/FindKim/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public string solution(string[] seoul) 
+    {
+        string answer = "";
+        for (int i = 0; i < seoul.Length; i++)
+        {
+            if (seoul[i] == "Kim")
+            { 
+                answer = $"김서방은 {i}에 있다";
+            }
+        }
+        return answer;
+    }
+    
+    static void Main()
+    {
+        
+    }
+}
---
+++ CSharp/Programmers/Lv1/FindPrimeNum/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public static int solution(int n)
+    {
+        bool[] filter = new bool[n + 1];    // true: 합성수, false: 소수
+        filter[0] = filter[1] = true;
+        
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (filter[i]) continue;
+            
+            for (int j = i * i; j <= n; j += i)
+            {
+                filter[j] = true;
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 2; i < n + 1; i++)
+        {
+            if (!filter[i]) answer++;
+        }
+        return answer;
+    }
+
+    static void Main()
+    {
+        solution(10);
+        solution(5);
+        solution(2);
+    }
+}
