61af52ba113883965a1c3f1d4206f52477a53613
정은채 <ddolchae513@gmail.com>
2025-11-20 13:28:17 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.62 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -1,29 +1,6 @@
 public class Solution {
     public string solution(string s) {
         string answer = "";
-
-        int count = 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (s.Substring(i, 1) == " ")
-            {
-                answer += " ";
-                count = 0;
-                continue;
-            }
-            
-            if (count % 2 == 0)
-            {
-                answer += s.Substring(i, 1).ToUpper();
-            }
-            else
-            {
-                answer += s.Substring(i, 1).ToLower();
-            }
-
-            count++;
-        }
-        
         return answer;
     }
 }
