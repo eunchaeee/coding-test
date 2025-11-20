b013b60af8cc4e8b368dc9284d590592ffcac162
정은채 <ddolchae513@gmail.com>
2025-11-20 09:45:54 +0900

    [level 1] Title: 소수 찾기, Time: 5.18 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.cs
@@ -1,29 +1,24 @@
 public class Solution {
     public int solution(int n) 
     {
-        int answer = 0;
-        bool[] filter = new bool[n + 1];
-
-        for (int i = 0; i < n + 1; i++)
+        bool[] filter = new bool[n + 1];    // true: 합성수, false: 소수
+        filter[0] = filter[1] = true;
+        
+        for (int i = 2; i * i <= n; i++)
         {
-            filter[i] = true;
-        }
-
-        for (int i = 2; i < n + 1; i++)
-        {
-            int factor = 2;
-            while (factor * i < n + 1)
+            if (filter[i]) continue;
+            
+            for (int j = i * i; j <= n; j += i)
             {
-                filter[factor * i] = false;
-                factor++;
+                filter[j] = true;
             }
         }
-
+        
+        int answer = 0;
         for (int i = 2; i < n + 1; i++)
         {
-            if (filter[i]) answer++;
+            if (!filter[i]) answer++;
         }
-        
         return answer;
     }
 }
