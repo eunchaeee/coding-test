babdd1a1800a4b385216a0d941b7614566d003f5
eunchae <ddolchae513@gmail.com>
2025-11-20 15:29:39 +0900

    [프로그래머스] [Lv.1] [C#] 정수 제곱근 판별

---
+++ CSharp/Programmers/Lv1/CheckSquareRoot/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public static long solution(long n) {
+        double sqrt = Math.Sqrt(n);
+        if (sqrt % 1 == 0)
+        {
+            return (long)Math.Pow(sqrt + 1, 2);
+        }
+        return -1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(solution(121));
+        Console.WriteLine(solution(3));
+    }
+}
