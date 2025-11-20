4fd681be23460b8760db696655b14120ab3861ae
eunchae <ddolchae513@gmail.com>
2025-11-20 13:34:59 +0900

    [프로그래머스] [Lv.1] [C#] 자릿수더하기

---
+++ CSharp/Programmers/Lv1/SumEach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        string s = n.ToString();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int.TryParse(s.Substring(i, 1), out int x);
+            answer += x;
+        }
+        
+        return answer;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(solution(123));
+        Console.WriteLine(solution(987));
+    }
+}
