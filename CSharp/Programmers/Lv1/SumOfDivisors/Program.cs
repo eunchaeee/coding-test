9319db92620244cedf5a7def3e38f8cccc1b1629
eunchae <ddolchae513@gmail.com>
2025-11-20 11:33:24 +0900

    [프로그래머스] [Lv.1] [C#] 약수의 합
    약수/divisor관련은 i*i <= n까지 돌아도 된다.

---
+++ CSharp/Programmers/Lv1/SumOfDivisors/Program.cs
@@ -0,0 +1,59 @@
+﻿public class Solution {
+    
+    // 단점:
+    // 소수일 경우 처음부터 끝까지 다 돌아야함. (시간복잡도 O(n))
+    // filter라는 추가적인 메모리까지 사용해야함.
+    public static int solution(int n) {
+        int answer = 0;
+        bool[] filter = new bool[n+1];
+
+        if (n <= 1) return n;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            if (filter[i]) break;
+
+            int apart = n / i;
+            if (n % i == 0)
+            {
+                filter[i] = true;
+                answer += i;
+                
+                if (i != apart)
+                {
+                    filter[apart] = true;
+                    answer += apart;
+                }
+            }
+        }
+        
+        return answer;
+    }
+
+    // 시간복잡도 O(루트N)
+    public static int solution2(int n)
+    {
+        int answer = 0;
+        
+        for (int i = 1; i*i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                answer += i;
+                
+                int apart = n / i;
+                if (i != apart)
+                {
+                    answer += apart;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine(solution(4));
+    }
+}
