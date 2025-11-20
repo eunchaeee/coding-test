7b1b585bc52d1829ffb6d5fd78173e18f32a8d83
정은채 <ddolchae513@gmail.com>
2025-11-20 14:20:10 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 4.15 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cs
@@ -1,19 +1,17 @@
 using System;
 using System.Linq;
+using System.Text;
 
 public class Solution {
     public long solution(long n) {
         int[] arr = n.ToString().Select(c => c - '0').ToArray();
         Array.Sort(arr);
-        string ordered = "";
+
+        var sb = new StringBuilder();
         for (int i = arr.Length - 1; i >= 0; i--)
         {
-            ordered += arr[i];
+            sb.Append(arr[i]);
         }
-        
-        long answer = 0;
-        long.TryParse(ordered, out answer);
-        
-        return answer;
+        return long.Parse(sb.ToString());
     }
 }
