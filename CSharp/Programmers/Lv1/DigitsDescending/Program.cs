2a75ba0b5cfbb7d7a23a51b6738164e9dc8f9841
eunchae <ddolchae513@gmail.com>
2025-11-20 14:21:31 +0900

    [프로그래머스] [Lv.1] [C#] 정수 내림차순으로 배치하기

---
+++ CSharp/Programmers/Lv1/DigitsDescending/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+public class Solution {
+    public static long solution(long n) 
+    {
+        int[] arr = n.ToString().Select(c => c - '0').ToArray();
+        Array.Sort(arr);
+
+        var sb = new StringBuilder();
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            sb.Append(arr[i]);
+        }
+        return long.Parse(sb.ToString());
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(solution(118372));
+    }
+}
