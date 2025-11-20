7af2ff9f6bc8e535444011b8dc5e311d78fa0e5e
정은채 <ddolchae513@gmail.com>
2025-11-20 10:12:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 76.04 ms, Memory: 47.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+                answer += "수";
+            else
+                answer += "박";
+        }
+        return answer;
+    }
+}
