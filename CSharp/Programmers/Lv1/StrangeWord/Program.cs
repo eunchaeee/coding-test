8f3a647c2f5bd8e82cc9b6dbc4c010502f923ee0
eunchae <ddolchae513@gmail.com>
2025-11-20 13:28:28 +0900

    [프로그래머스] [Lv.1] [C#] 이상한 문자 만들기

---
+++ CSharp/Programmers/Lv1/StrangeWord/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public static string solution(string s) {
+        string answer = "";
+
+        int count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s.Substring(i, 1) == " ")
+            {
+                answer += " ";
+                count = 0;
+                continue;
+            }
+            
+            if (count % 2 == 0)
+            {
+                answer += s.Substring(i, 1).ToUpper();
+            }
+            else
+            {
+                answer += s.Substring(i, 1).ToLower();
+            }
+
+            count++;
+        }
+        
+        Console.WriteLine(answer);
+        return answer;
+    }
+
+    static void Main()
+    {
+        solution("try hello world");
+    }
+}
