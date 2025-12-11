15ef058d7788199961917eee42f38319de6f45f7
eunchae <ddolchae513@gmail.com>
2025-12-11 11:50:04 +0900

    [LeetCode] [C#] 191_NumberOf1Bits

---
+++ CSharp/LeetCode/191_NumberOf1Bits/191_NumberOf1Bits/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution {
+    public int HammingWeight(int n)
+    {
+        int answer = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            if (((n >> i) & 1) == 1)
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public int HammingWeight2(int n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            n &= (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.HammingWeight(2147483645));
+    }
+}
