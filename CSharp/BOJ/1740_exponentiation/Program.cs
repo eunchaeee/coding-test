f126300a34077dd7dc964b34a570b9fc048bf705
eunchae <ddolchae513@gmail.com>
2025-12-12 01:19:02 +0900

    [BOJ] [C#] 1740. 거듭제곱
    Math.Pow는 쓰지말자...!

---
+++ CSharp/BOJ/1740_exponentiation/Program.cs
@@ -0,0 +1,22 @@
+﻿class Solution
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        long answer = 0;
+        
+        long pow = 1;
+        while (n > 0)
+        {
+            if ((n & 1L) == 1L)
+            {
+                answer += pow;
+            }
+
+            pow *= 3;
+            n >>= 1;
+        }
+        
+        Console.WriteLine(answer);
+    }
+}
