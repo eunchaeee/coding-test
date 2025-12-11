d20bfe2de1584309c62e5d8ba65135301a275e02
eunchae <ddolchae513@gmail.com>
2025-12-12 00:45:25 +0900

    [LeetCode] [C#] 268. Missing Number

---
+++ CSharp/LeetCode/268_MissingNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution 
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length;
+        int answer = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            answer ^= (i + 1);
+            answer ^= nums[i];
+        }
+
+        return answer;
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        int[] nums = new[] { 9,6,4,2,3,5,7,0,1};
+        Console.WriteLine(sol.MissingNumber(nums));
+    }
+}
