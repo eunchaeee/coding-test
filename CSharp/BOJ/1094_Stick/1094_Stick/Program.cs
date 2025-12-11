d54497f1b9291eda2720daffaeafd96c6f79c0fa
eunchae <ddolchae513@gmail.com>
2025-12-11 17:36:19 +0900

    [BOJ] [C#] 1094_Stick

---
+++ CSharp/BOJ/1094_Stick/1094_Stick/Program.cs
@@ -0,0 +1,15 @@
+﻿class Solution
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int count = 0;
+        
+        while (x != 0)
+        {
+            x &= (x - 1);
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
