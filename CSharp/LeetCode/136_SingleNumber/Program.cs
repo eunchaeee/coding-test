67a8f0e5c4fa7c800ec151d647e29bda55746c80
eunchae <ddolchae513@gmail.com>
2025-12-12 00:37:32 +0900

    [LeetCode] [C#] 136. Single Number

---
+++ CSharp/LeetCode/136_SingleNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution {
+    public int SingleNumber(int[] nums)
+    {
+        Dictionary<int, bool> dict = new Dictionary<int, bool>();
+
+        foreach (var i in nums)
+        {
+            if (dict.ContainsKey(i))
+            {
+                dict.Remove(i);
+            }
+            else
+            {
+                dict[i] = true;
+            }
+        }
+
+        return dict.Keys.First();
+    }
+
+    public int SingleNumber2(int[] nums)
+    {
+        int answer = 0;
+        foreach (var n in nums)
+        {
+            answer ^= n;
+        }
+
+        return answer;
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        int[] nums = new int[] { 4,1,2,1,2 };
+        Console.WriteLine(sol.SingleNumber2(nums));
+    }
+}
