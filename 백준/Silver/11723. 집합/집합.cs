66983792b974400b45765803d4e4d4e97d823ae7
정은채 <ddolchae513@gmail.com>
2025-12-12 10:46:25 +0900

    [Silver V] Title: 집합, Time: 1516 ms, Memory: 43136 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cs
+++ 백준/Silver/11723. 집합/집합.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Text;
 
 class Program
@@ -7,37 +5,42 @@ class Program
     static void Main()
     {
         int m = int.Parse(Console.ReadLine());
-        HashSet<int> s = new HashSet<int>();
-
+        int bit = 0;
         StringBuilder sb = new StringBuilder();
-        
+
         for (int i = 0; i < m; i++)
         {
             string[] parts = Console.ReadLine().Split();
             string command = parts[0];
-            
-            switch (command)
+
+            if (command == "add")
             {
-                case "add":
-                    s.Add(int.Parse(parts[1]));
-                    break;
-                case "remove":
-                    s.Remove(int.Parse(parts[1]));
-                    break;
-                case "check":
-                    sb.AppendLine((s.Contains(int.Parse(parts[1]))? "1": "0"));
-                    break;
-                case "toggle":
-                    if (s.Contains(int.Parse(parts[1]))) s.Remove(int.Parse(parts[1]));
-                    else s.Add(int.Parse(parts[1]));
-                    break;
-                case "all":
-                    for (int j = 1; j <= 20; j++)
-                        s.Add(j);
-                    break;
-                case "empty":
-                    s = new HashSet<int>() { };
-                    break;
+                int x = int.Parse(parts[1]);
+                bit |= (1 << x);
+            }
+            else if (command == "remove")
+            {
+                int x = int.Parse(parts[1]);
+                bit &= ~(1 << x);
+            }
+            else if (command == "check")
+            {
+                int x = int.Parse(parts[1]);
+                string s = ((bit >> x) & 1).ToString();
+                sb.AppendLine(s);
+            }
+            else if (command == "toggle")
+            {
+                int x = int.Parse(parts[1]);
+                bit ^= (1 << x);
+            }
+            else if (command == "all")
+            {
+                bit = (1 << 21) - 2;
+            }
+            else
+            {
+                bit = 0;
             }
         }
         
