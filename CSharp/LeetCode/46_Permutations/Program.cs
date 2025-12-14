f2ac6aaeb852190df4ccd60617590bd5b06c9dea
eunchae <ddolchae513@gmail.com>
2025-12-15 00:21:48 +0900

    [LeetCode] [C#] 46. Permutations

---
+++ CSharp/LeetCode/46_Permutations/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution 
+{
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var path = new List<int>();
+        var used = new bool[nums.Length];
+
+        DFS(nums, used, path, result);
+        return result;
+    }
+
+    public void DFS(int[] nums, bool[] used, List<int> path, List<IList<int>> result)
+    {
+        if (path.Count == nums.Length)
+        {
+            result.Add(new List<int>(path));
+            return;
+        }
+        
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (used[i]) continue;
+
+            used[i] = true;
+            path.Add(nums[i]);
+            
+            DFS(nums, used, path, result);
+
+            used[i] = false;
+            path.Remove(nums[i]);
+        }
+    }
+}
