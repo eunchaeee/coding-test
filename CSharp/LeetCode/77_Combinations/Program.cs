cba0a5c7555711e09bd4cba6de3a5f5f7b16a457
eunchae <ddolchae513@gmail.com>
2025-12-14 23:16:00 +0900

    [LeetCode] [C#] 77. Combinations

---
+++ CSharp/LeetCode/77_Combinations/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * 1부터 시작해서
+ * 하나를 고르고
+ * 그 다음 숫자부터 다시 고르고
+ * k개가 되면 저장하고
+ * 다시 돌아와서 다른 선택을 해본다.
+ */
+
+public class Solution
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        var result = new List<IList<int>>();
+        var path = new List<int>(k);
+        DFS(1, n, k, path, result);
+        return result;
+    }
+
+    void DFS(int start, int n, int k, List<int> path, List<IList<int>> result)
+    {
+        // 종료 조건
+        if (path.Count == k)
+        {
+            result.Add(new List<int>(path));
+            return;
+        }
+        
+        // 다음 숫자 선택
+        for (int i = start; i <= n; i++)
+        {
+            path.Add(i);
+            DFS(i + 1, n, k, path, result);
+            path.RemoveAt(path.Count - 1);  
+        }
+    }
+
+    public static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.Combine(4, 2));
+    }
+}
