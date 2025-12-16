2b3d6f051528cba259a46979d02605fd49510542
eunchae <ddolchae513@gmail.com>
2025-12-16 17:46:59 +0900

    [BOJ] [C#] 1181. 단어 정렬

---
+++ CSharp/BOJ/1181_SortWords/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> wordsSet = new HashSet<string>();
+        
+        for (int i = 0; i < n; i++)
+        {
+            wordsSet.Add(Console.ReadLine());
+        }
+
+        string[] words = new string[wordsSet.Count];
+        wordsSet.CopyTo(words);
+        
+        Array.Sort(words, (a, b) =>
+        {
+            int x = a.Length.CompareTo(b.Length);
+            if (x != 0) return x;
+            return a.CompareTo(b);
+        });
+
+        for (int i = 0; i < wordsSet.Count; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+    }
+}
