b2cd40c9dca8eaccf0d3170a7289104bedea2015
eunchae <ddolchae513@gmail.com>
2025-12-16 17:14:02 +0900

    [프로그래머스] [C#] 12915. 문자열 내 마음대로 정렬하기

---
+++ CSharp/Programmers/12915_SortString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+public class Solution {
+    public string[] solution(string[] strings, int n) {
+        List<string> answer = new ();
+        List<string>[] sort = new List<string>[26];
+
+        for (int i = 0; i < 26; i++)
+        {
+            sort[i] = new List<string>();
+        }
+        
+        foreach (var s in strings)
+        {
+            sort[s[n] -'a'].Add(s);
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            sort[i].Sort();
+            answer.AddRange(sort[i]);
+        }
+        
+        
+        return answer.ToArray();
+    }
+
+    public string[] solution2(string[] strings, int n)
+    {
+        Array.Sort(strings, (a, b) =>
+        {
+            int c = a[n].CompareTo(b[n]);
+            if (c != 0) return c;
+            return a.CompareTo(b);
+        });
+        return strings;
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        sol.solution(new string[] { "sun", "bed", "car" }, 1);
+    }
+}
