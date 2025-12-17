676080a09a722faeb233f2f31ab706069b2b4a55
eunchae <ddolchae513@gmail.com>
2025-12-17 17:47:46 +0900

    [백준] [C#] 10816. 숫자카드2

---
+++ CSharp/BOJ/10816_DigitCard2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        int n = int.Parse(Console.ReadLine());
+        var nums = Console.ReadLine().Split();
+
+ 
+        foreach (var num in nums)
+        {
+            dict[num] = dict.GetValueOrDefault(num, 0) + 1;
+        }
+        
+
+        StringBuilder sb = new StringBuilder();
+        int m = int.Parse(Console.ReadLine());
+        var inputs = Console.ReadLine().Split();
+        
+        foreach (var input in inputs)
+        {
+            dict.TryGetValue(input, out int value);
+            sb.Append(value);
+            sb.Append(" ");
+        }
+        
+        Console.Write(sb.ToString());
+    }
+}
