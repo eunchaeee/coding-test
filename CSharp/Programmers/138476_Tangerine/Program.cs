4a0cf5650ef1ed03c37f4aa0908fa7fb3af7921a
eunchae <ddolchae513@gmail.com>
2025-12-17 15:41:57 +0900

    [프로그래머스] [C#] 138476_귤고르기

---
+++ CSharp/Programmers/138476_Tangerine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int k, int[] tangerine)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < tangerine.Length; i++)
+        {
+            dict[tangerine[i]] = dict.GetValueOrDefault(tangerine[i], 0) + 1;
+        }
+
+        List<int> nums = new List<int>();
+        nums = dict.Values.ToList();
+        nums.Sort();
+
+        int off = tangerine.Length - k;
+
+        int idx = 0;
+        for (int i = 0; i < off; i++)
+        {
+            if (nums[idx] > 0)
+            {
+                nums[idx]--;
+            }
+            else
+            {
+                idx += 1;
+                nums[idx]--;
+            }
+        }
+
+        return nums.Count(x => x > 0);
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution(6, new int[] {1, 3, 2, 5, 4, 5, 2, 3}));
+    }
+}
