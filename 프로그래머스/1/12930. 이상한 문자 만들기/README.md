6dd7a42c55d190c0f7bcaa85b720b0639da6dbc0
정은채 <ddolchae513@gmail.com>
2025-12-17 09:33:44 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.95 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
 
 ### 성능 요약
 
-메모리: 31.3 MB, 시간: 0.62 ms
+메모리: 31.3 MB, 시간: 0.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 20일 13:28:16
+2025년 12월 17일 09:33:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -1,6 +1,24 @@
+using System.Text;
+
 public class Solution {
-    public string solution(string s) {
-        string answer = "";
-        return answer;
+    public string solution(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (string x in s.Split())
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+
+                string c = i % 2 == 1 ? x[i].ToString().ToLower() : x[i].ToString().ToUpper();
+                sb.Append(c);
+            }
+
+            sb.Append(" ");
+        }
+        
+        sb.Remove(sb.Length - 1, 1);
+
+        return sb.ToString();
     }
 }
