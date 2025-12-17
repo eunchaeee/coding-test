0947822be5049a6efb19103a714c44dd9600cc51
eunchae <ddolchae513@gmail.com>
2025-12-17 11:32:35 +0900

    [프로그래머스] [C#] 12951. JadenCase

---
+++ CSharp/Programmers/12951_JadenCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+public class Solution 
+{
+    public string solution(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 0;
+        foreach (char c in s)
+        {
+            if (c == ' ')
+            {
+                sb.Append(' ');
+                idx = 0;
+                continue;
+            }
+
+            if (idx == 0 && char.IsLetter(c))
+            {
+                sb.Append(char.ToUpper(c));
+            }
+            else
+            {
+                sb.Append(char.ToLower(c));
+            }
+
+            idx++;
+        }
+        
+        return sb.ToString();
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution("3people uns2Followed me"));
+    }
+}
