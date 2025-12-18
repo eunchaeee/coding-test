137b362a7f8761780d42d5b3668899142b006a3e
eunchae <ddolchae513@gmail.com>
2025-12-18 14:21:04 +0900

    [프로그래머스] [C#] 42842. 카펫

---
+++ CSharp/Programmers/42842_Carpet/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Principal;
+
+public class Solution
+{
+    public int[] solution(int brown, int yellow)
+    {
+        int[] answer = new int[] { };
+        
+        int total = brown + yellow;
+
+        List<int[]> divider = new List<int[]>();
+
+
+        for (int i = 1; i * i <= total; i++)
+        {
+            if (total%i == 0)
+            {
+                divider.Add(new int[] { total / i, i });
+            }
+        }
+
+        foreach (int[] pair in divider)
+        {
+            int sum = pair[0] + pair[1];
+            if (sum == brown/2 + 2)
+            {
+                answer = pair;
+            }
+        }
+        
+        return answer;
+    }
+
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(string.Join(" ", solution.solution(24, 24)));
+    }
+}
