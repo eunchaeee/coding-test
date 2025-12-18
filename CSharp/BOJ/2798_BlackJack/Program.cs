03352f1f0b4af9110f311a79870544e6f983b530
eunchae <ddolchae513@gmail.com>
2025-12-18 16:47:10 +0900

    [백준] [C#] 2798. 체육복

---
+++ CSharp/BOJ/2798_BlackJack/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var parts = Console.ReadLine().Split();
+        int n = int.Parse(parts[0]);
+        int m = int.Parse(parts[1]);
+
+        var cards = Console.ReadLine().Split().Select(x => int.Parse(x)).ToList();
+        
+        cards.Sort();
+
+        int maxSum = 0;
+        
+        for (int i = 0; i < n - 2; i++)
+        {
+            for (int j = i + 1; j < n - 1; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    int sum = cards[i] + cards[j] + cards[k];
+
+                    if (sum == m)
+                    {
+                        Console.WriteLine(sum);
+                        return;
+                    }
+
+
+                    if (sum > m)
+                        break;
+                    
+                    if (maxSum < sum) maxSum = sum;
+                }
+            }
+        }
+        
+        Console.WriteLine(maxSum);
+    }
+}
