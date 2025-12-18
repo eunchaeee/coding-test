540e508b17a2ca4528b69d87a190135c4cadf4f9
정은채 <ddolchae513@gmail.com>
2025-12-18 14:52:23 +0900

    [level 2] Title: 피로도, Time: 3.32 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int k, int[,] dungeons) {
+        int answer = -1;
+
+        List<int> path = new List<int>();
+        bool[] visited = new bool[dungeons.GetLength(0)];
+        answer = DFS(k, path, visited, dungeons);
+        
+        return answer;
+    }
+
+    public int DFS(int k, List<int> path, bool[] visited, int[,] dungeons)
+    {
+        int maxCount = path.Count;
+        
+        for (int i = 0; i < dungeons.GetLength(0); i++)
+        {
+            if (visited[i]) continue;
+
+            int need = dungeons[i, 0];
+            int cost = dungeons[i, 1];
+            
+            if (k >= need && k - cost >= 0)
+            {
+                path.Add(i);
+                visited[i] = true;
+                k -= cost;
+                
+                maxCount = Math.Max(maxCount, DFS(k, path, visited, dungeons));
+                
+                path.RemoveAt(path.Count - 1);
+                visited[i] = false;
+                k += cost;
+            }
+        }
+
+        return maxCount;
+    }
+}
