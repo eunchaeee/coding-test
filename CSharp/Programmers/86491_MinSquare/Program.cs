49807b58caa41fa2864ec3312a5fd04ccbb39d13
eunchae <ddolchae513@gmail.com>
2025-12-19 10:08:59 +0900

    [프로그래머스] [C#] 최소 직사각형

---
+++ CSharp/Programmers/86491_MinSquare/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int[,] sizes) {
+        int answer = 0;
+
+        int maxMax = 0;
+        int minMax = 0;
+        
+        for (int i = 0; i < sizes.GetLength(0); i++)
+        {
+            int min = sizes[i, 0] >= sizes[i, 1]? sizes[i, 1] : sizes[i, 0];
+            int max = sizes[i, 0] >= sizes[i, 1]? sizes[i, 0] : sizes[i, 1];
+
+            if (min > minMax) minMax = min;
+            if (max > maxMax) maxMax = max;
+        }
+        
+        return maxMax * minMax;
+    }
+}
