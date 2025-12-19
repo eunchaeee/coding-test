eac747fb0b1713adbbf0b69a5b81cf412fa8dad1
정은채 <ddolchae513@gmail.com>
2025-12-19 09:44:23 +0900

    [level 1] Title: 예산, Time: 2.43 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 2.32 ms
+메모리: 31.4 MB, 시간: 2.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 15:16:48
+2025년 12월 19일 09:44:22
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.cs
+++ 프로그래머스/1/12982. 예산/예산.cs
@@ -3,18 +3,15 @@ using System;
 public class Solution {
     public int solution(int[] d, int budget) {
         Array.Sort(d);
-        int count = 0;
+
         for (int i = 0; i < d.Length; i++)
         {
-            if (budget - d[i] < 0)
-            {
-                break;
-            }
-
             budget -= d[i];
-            count++;
+            if (budget < 0)
+            {
+                return i;
+            }
         }
-        return count;
+        return d.Length;
     }
-
 }
