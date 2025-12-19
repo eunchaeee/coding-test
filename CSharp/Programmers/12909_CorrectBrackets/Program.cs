0370d5c9bd9af08c63ce76f4801af320e1e8c18f
eunchae <ddolchae513@gmail.com>
2025-12-19 15:14:35 +0900

    [프로그래머스] [C#] 올바른괄호

---
+++ CSharp/Programmers/12909_CorrectBrackets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public bool solution(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (c == '(')
+                stack.Push(c);
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+                stack.Pop();
+            }
+        }
+        return stack.Count == 0;
+    }
+
+    public bool solution2(string s)
+    {
+        int total = 0;
+
+        foreach (var c in s)
+        {
+            if (c == '(')
+                total++;
+            else 
+                total--;
+
+            if (total < 0) return false;
+        }
+
+        return total == 0;
+    }
+}
