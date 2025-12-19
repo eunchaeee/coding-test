f6cc053d5506c1278be940046d61a2be2c61d18b
eunchae <ddolchae513@gmail.com>
2025-12-19 09:44:56 +0900

    [프로그래머스] [C#] 예산

---
+++ CSharp/Programmers/12982_Budget/12982_Budget/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Solution {
+    public int solution(int[] d, int budget) {
+        Array.Sort(d);
+
+        for (int i = 0; i < d.Length; i++)
+        {
+            budget -= d[i];
+            if (budget < 0)
+            {
+                return i;
+            }
+        }
+        return d.Length;
+    }
+}
