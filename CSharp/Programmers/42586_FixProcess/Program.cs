81043c1f9d4566cdbffe860d1d41a68b9e1b1b29
eunchae <ddolchae513@gmail.com>
2025-12-19 16:45:05 +0900

    [프로그래머스] [C#] 기능개발

---
+++ CSharp/Programmers/42586_FixProcess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Solution {
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        int n = progresses.Length;
+        List<int> result = new List<int>();
+
+        int[] days = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int remain = 100 - progresses[i];
+            days[i] = (remain + speeds[i] - 1) / speeds[i];
+        }
+
+        List<int> answer = new List<int>();
+        int count = 1;
+        int current = days[0];
+        
+        for (int i = 1; i < n; i++)
+        {
+            if (days[i] <= current)
+            {
+                count++;
+            }
+            else
+            {
+                answer.Add(count);
+                current = days[i];
+                count = 1;
+            }
+            Console.WriteLine($"i : {i}, n: {n}");
+        }
+        
+        answer.Add(count);
+
+        return answer.ToArray();
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        var s =string.Join(", ", sol.solution(new int[] { 93, 30, 55 }, new int[] { 1, 30, 5 }));
+        Console.WriteLine(s);
+    }
+}
