858a4808f1505545d8dec0522a13365ac5089953
eunchae <ddolchae513@gmail.com>
2025-12-20 10:21:56 +0900

    [프로그래머스] [C#] 42587. 프로세스

---
+++ CSharp/Programmers/42587_Process/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Solution  
+{
+    public int solution(int[] priorities, int location)
+    {
+        List<int> pList = new List<int>();
+        int n = priorities.Length;
+        List<int[]> pq = new List<int[]>();
+        bool[] finished = new bool[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            pq.Add(new int[] { i, priorities[i] });
+        }
+        
+        pq.Sort((a, b) => b[1].CompareTo(a[1]));
+        
+        int count = 0;
+        while (!finished[location])
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (finished[i]) continue;
+                if (pq[count][1] > priorities[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    finished[i] = true;
+                    count++;
+                    if (i == location) break;
+                }
+            }
+        }
+        Console.WriteLine(count);
+        return count;
+    }
+
+    static void Main()
+    {
+        Solution sol = new Solution();
+        sol.solution(new int[] { 2, 1, 3, 2 }, 2);
+        sol.solution(new int[] { 1, 1, 9, 1, 1, 1 }, 0);
+    }
+}
